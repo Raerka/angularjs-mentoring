d6e5c28b175b98aeff02805f25bf7e3f966e242d
Andrei Kasmykou <andrei_kasmykou@epam.com>
2019-03-22 23:14:42 +0300

    Refactoring

--- src/app/modules/add-course/add-course.module.ts
+++ src/app/modules/add-course/add-course.module.ts
@@ -7,93 +7,16 @@ import { CourseDurationComponent } from './course-duration/course-duration.compo
 import { CourseAuthorsComponent } from './course-authors/course-authors.component';
 import { CourseDateComponent } from './course-date/course-date.component';
 import { PipesModule } from '../../pipes/pipes.module';
-
-
-
-
-import {
-  MatAutocompleteModule,
-  // MatBadgeModule,
-  // MatBottomSheetModule,
-  // MatButtonModule,
-  // MatButtonToggleModule,
-  // MatCardModule,
-  // MatCheckboxModule,
-  MatChipsModule,
-  // MatDatepickerModule,
-  // MatDialogModule,
-  // MatDividerModule,
-  // MatExpansionModule,
-  // MatGridListModule,
-  MatIconModule,
-  // MatInputModule,
-  // MatListModule,
-  // MatMenuModule,
-  // MatNativeDateModule,
-  // MatPaginatorModule,
-  // MatProgressBarModule,
-  // MatProgressSpinnerModule,
-  // MatRadioModule,
-  // MatRippleModule,
-  // MatSelectModule,
-  // MatSidenavModule,
-  // MatSliderModule,
-  // MatSlideToggleModule,
-  // MatSnackBarModule,
-  // MatSortModule,
-  // MatStepperModule,
-  // MatTableModule,
-  // MatTabsModule,
-  // MatToolbarModule,
-  // MatTooltipModule,
-  // MatTreeModule,
-} from '@angular/material';
-
+import { MatAutocompleteModule, MatChipsModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   exports: [
     MatAutocompleteModule,
-    // MatBadgeModule,
-    // MatBottomSheetModule,
-    // MatButtonModule,
-    // MatButtonToggleModule,
-    // MatCardModule,
-    // MatCheckboxModule,
     MatChipsModule,
-    // MatStepperModule,
-    // MatDatepickerModule,
-    // MatDialogModule,
-    // MatDividerModule,
-    // MatExpansionModule,
-    // MatGridListModule,
     MatIconModule,
-    // MatInputModule,
-    // MatListModule,
-    // MatMenuModule,
-    // MatNativeDateModule,
-    // MatPaginatorModule,
-    // MatProgressBarModule,
-    // MatProgressSpinnerModule,
-    // MatRadioModule,
-    // MatRippleModule,
-    // MatSelectModule,
-    // MatSidenavModule,
-    // MatSliderModule,
-    // MatSlideToggleModule,
-    // MatSnackBarModule,
-    // MatSortModule,
-    // MatTableModule,
-    // MatTabsModule,
-    // MatToolbarModule,
-    // MatTooltipModule,
-    // MatTreeModule,
   ]
 })
 export class DemoMaterialModule {}
-
-
-
-
 
 @NgModule({
   declarations: [
